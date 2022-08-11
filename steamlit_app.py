e126f6bfc81ed1662c56399108ae638002138d10
Jon Killen <110849661+jckillen@users.noreply.github.com>
2022-08-11 12:43:16 -0500

    Update steamlit_app.py
    Added new package (requests) and text below table

--- steamlit_app.py
+++ steamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit
 import pandas
+import requests
 
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
@@ -22,3 +24,5 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
 
+streamlit.header("Fruityvice Fruit Advice!")
+streamlit.text(fruityvice_response.json())
